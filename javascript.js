1ed434c81909df6b1c7d26d1b3a9da30fc31d672
Madeline0421 <madelinehou421@gmail.com>
2022-12-28 02:01:01 -0500

    Add playRound and game method

--- javascript.js
+++ javascript.js
@@ -8,6 +8,8 @@ Breaking down goal:
 - Compare user vs computer's choice, announce result
 */
 choices = Array("Rock", "Paper", "Scissors");
+let gamesWonByUser = 0;
+let gamesWonByComputer = 0;
 
 function getComputerChoice() {     
     return choices[Math.floor(Math.random() * choices.length)];
@@ -17,5 +19,66 @@ function getUserChoice() {
     return prompt("Choose between Rock, Paper, or Scissors.");
 }
 
+function playRound(playerSelection, computerSelection) {
+    /*
+    rock > scissors > paper > rock 
+    */
 
+    if (playerSelection.toLowerCase() == "rock") { 
+        switch (computerSelection) {
+            case "Rock": 
+                return "You both tied using Rock!";
+            case "Scissors": 
+                gamesWonByUser++;
+                return "You win! Rock beats Scissors!";
+            case "Paper":
+                gamesWonByComputer++;
+                return "You lose! Paper beats Rock!";
+        }
+    }
+    else if (playerSelection.toLowerCase() == "paper") { 
+        switch (computerSelection) {
+            case "Paper": 
+                return "You both tied using Paper!";
+            case "Rock": 
+                gamesWonByUser++;
+                return "You win! Paper beats Rock!";
+            case "Scissors":
+                gamesWonByComputer++;
+                return "You lose! Scissors beats Paper!";
+        }
+    }
+    else if (playerSelection.toLowerCase() == "scissors") { 
+        switch (computerSelection) {
+            case "Scissors": 
+                return "You both tied using Scissors!";
+            case "Paper": 
+                gamesWonByUser++;
+                return "You win! Scissors beats Paper!";
+            case "Rock":
+                gamesWonByComputer++;
+                return "You lose! Rock beats Scissors!";
+        }
+    }
+}
 
+function game() { 
+    for (let i = 0; i < 5; i++) { 
+        console.log(playRound(getUserChoice(), getComputerChoice()));
+        console.log(`Current Score: 
+        User: ${gamesWonByUser}
+        Computer: ${gamesWonByComputer}`);
+    }
+    console.log(`Final Score: 
+    User: ${gamesWonByUser}
+    Computer: ${gamesWonByComputer}`);
+    if (gamesWonByUser > gamesWonByComputer) { 
+        return "You won!";
+    } else if (gamesWonByUser < gamesWonByComputer) {
+        return "You lost!";
+    } else { 
+        return "You tied.";
+    }
+}
+
+console.log(game());    
